ddafc7f4bbff2a1ae92c716e28375c5b63429d3b
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-10-05 02:39:19 +0300

    Lab№5.4

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab№5.4/Lab№5.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(Test)
+		{
+			int K = 1;
+			int N = 10;
+
+			Assert::AreEqual(S0(K, N), 3.81188, 0.00001);
+			Assert::AreEqual(S1(K, N, K), 3.81188, 0.00001);
+			Assert::AreEqual(S2(K, N, N), 3.81188, 0.00001);
+			Assert::AreEqual(S3(K, N, K, 0), 3.81188, 0.00001);
+			Assert::AreEqual(S4(K, N, N, 0), 3.81188, 0.00001);
+		}
+	};
+}
